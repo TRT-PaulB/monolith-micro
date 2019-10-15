b7a4e96f12a515df139e2d0d36abacd692b1c84c
TRT-PaulB <paul.bickell@raytheon.co.uk>
2019-10-15 16:57:45 +0100

    registering a service from a client app

--- pb-ecommerce-product-api/src/main/java/com/micro/pbecommerceproductapi/PbEcommerceProductApiApplication.java
+++ pb-ecommerce-product-api/src/main/java/com/micro/pbecommerceproductapi/PbEcommerceProductApiApplication.java
@@ -2,8 +2,10 @@ package com.micro.pbecommerceproductapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PbEcommerceProductApiApplication {
 
 	public static void main(String[] args) {
